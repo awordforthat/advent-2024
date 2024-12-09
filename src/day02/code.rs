8702c3147eec9d3f0048c6cd9315c85bfadbbebc
Emily Charles <emily.w.charles@gmail.com>
2024-12-08 23:37:08 -0500

    day two part two - kind of gross solution

--- src/day02/code.rs
+++ src/day02/code.rs
@@ -2,6 +2,23 @@ use std::io::{self};
 
 use crate::utils::helpers::{get_file_as_lines, parse_str_as_ints};
 
+fn is_line_safe(line:&Vec<i32>) -> bool {
+    let mut last:&i32 = &line[0];
+    let mut last_delta_sign:i32 = if last - &line[1] > 0 {-1} else {1};
+    for next in &line[1..] {
+
+        let delta = last - next;
+        let delta_sign: i32 = if last - next > 0 {-1} else {1};
+        if (delta.abs() > 3 || delta.abs() == 0) ||(delta_sign != last_delta_sign) {
+            return false;
+        }
+        last_delta_sign = delta_sign;
+        last = next;
+    }
+    return true;
+    
+}
+
 pub fn a() -> io::Result<()> {
 
     let absolute_path = std::env::current_dir()?.join("src/day02/data.txt");
@@ -10,22 +27,42 @@ pub fn a() -> io::Result<()> {
     for line in &lines {
         let ints: Vec<i32> = parse_str_as_ints(line);
 
-        let mut last:&i32 = &ints[0];
-        let mut last_delta_sign:i32 = if last - &ints[1] > 0 {-1} else {1};
-        let mut line_is_safe:bool = true;
-        for next in &ints[1..] {
-
-            let delta = last - next;
-            let delta_sign: i32 = if last - next > 0 {-1} else {1};
-            if (delta.abs() > 3 || delta.abs() == 0) ||(delta_sign != last_delta_sign) {
-                line_is_safe = false;
-                break
-            }
-            last_delta_sign = delta_sign;
-            last = next;
-        }
+        let line_is_safe = is_line_safe(&ints);
         total_safe_lines += if line_is_safe {1} else {0};
         
+    }
+    println!("Total safe lines: {}", total_safe_lines);
+
+    Ok(())
+}
+
+pub fn b() -> io::Result<()> {
+    let absolute_path = std::env::current_dir()?.join("src/day02/data.txt");
+    let lines:Vec<String> = get_file_as_lines(absolute_path)?;
+    let mut total_safe_lines = 0;
+    let mut failed_lines: Vec<Vec<i32>> = Vec::new();
+    for line in &lines {
+        let ints: Vec<i32> = parse_str_as_ints(line);
+        let line_is_safe = is_line_safe(&ints);
+        
+        total_safe_lines += if line_is_safe {1} else {0};
+        if !line_is_safe {
+            failed_lines.push(ints);
+        }
+        
+    }
+
+    for failed_line in failed_lines {
+       
+        for i in 0..failed_line.len() {
+            let mut copy = failed_line.clone();
+            copy.remove(i);
+            let trunc_is_safe = is_line_safe(&copy);
+            if trunc_is_safe {
+                total_safe_lines += 1;
+                break;
+            }
+        }
     }
     println!("Total safe lines: {}", total_safe_lines);
 
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ mod day02;
 mod utils;
 
 fn main() {
-    // let _ = day01::code::a();
-    // let _ = day01::code::b();
-    let _ = day02::code::a();
+    // let _ = day01::code::a(); // 1189304
+    // let _ = day01::code::b(); // 24349736
+    // let _ = day02::code::a(); // 202
+    let _ = day02::code::b(); // 271
 }
